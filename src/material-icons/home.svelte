cdb81617ea855dabdd4d0639df3a7f74e820b591
ru-museum <museum_ru@yahoo.co.jp>
2021-01-15 12:28:52 +0900

    Create home.svelte

---
+++ src/material-icons/home.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let size = "1em";
+  export let color = "currentColor";
+  export let viewBox = "0 0 24 24";
+</script>
+
+<svg fill={color} width="{size}" height="{size}" viewBox="{viewBox}"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/></svg>
