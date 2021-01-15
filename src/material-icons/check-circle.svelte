3e3cc2ef4a27729a3d1b484c38d7cef1ad680c98
ru-museum <museum_ru@yahoo.co.jp>
2021-01-15 12:18:22 +0900

    Create check-circle.svelte

---
+++ src/material-icons/check-circle.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let size = "1em";
+  export let color = "currentColor";
+  export let viewBox = "0 0 24 24";
+</script>
+<svg fill="{color}" width="{size}" height="{size}" viewBox="{viewBox}"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
