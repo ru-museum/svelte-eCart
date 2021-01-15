7b32126df5a0a31f2625f6acf3a689f834ec6ac5
ru-museum <museum_ru@yahoo.co.jp>
2021-01-15 12:29:44 +0900

    Create keyboard-arrow-up.svelte

---
+++ src/material-icons/keyboard-arrow-up.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let size = "1em";
+  export let color = "currentColor";
+  export let viewBox = "0 0 24 24";
+</script>
+
+<svg fill={color} width="{size}" height="{size}" viewBox="{viewBox}"><path d="M0 0h24v24H0z" fill="none"/><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/></svg>
