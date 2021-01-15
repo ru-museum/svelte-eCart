a08184677f3606df1058c74433c376c7535e7963
ru-museum <museum_ru@yahoo.co.jp>
2021-01-15 11:46:14 +0900

    Create arrow-downward.svelte

---
+++ src/material-icons/arrow-downward.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let size = "1em";
+  export let color = "currentColor";
+  export let viewBox = "0 0 24 24";
+</script>
+
+<svg fill={color} width="{size}" height="{size}" viewBox="{viewBox}"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"/></svg>
