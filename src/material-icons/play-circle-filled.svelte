394a8c8e22b21bae11df308ab82a386ad9de1e75
ru-museum <museum_ru@yahoo.co.jp>
2021-01-15 12:33:23 +0900

    Create play-circle-filled.svelte

---
+++ src/material-icons/play-circle-filled.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let size = "1em";
+  export let color = "currentColor";
+  export let viewBox = "0 0 24 24";
+</script>
+
+<svg fill={color} width="{size}" height="{size}" viewBox="{viewBox}"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/></svg>
