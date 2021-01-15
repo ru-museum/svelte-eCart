c4982c09b52d2fb1963fff8d33b2fbd27048e4e6
ru-museum <museum_ru@yahoo.co.jp>
2021-01-15 12:36:36 +0900

    Delete remove-box.svelte

--- src/material-icons/remove-box.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  export let size = "1em";
-  export let color = "currentColor";
-  export let viewBox = "0 0 24 24";
-</script>
-
-<svg fill={color} width="{size}" height="{size}" viewBox="{viewBox}"><g><rect fill="none" height="24" width="24"/></g><g><g><g><path d="M19,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V5C21,3.9,20.1,3,19,3z M17,13H7v-2h10V13z"/></g></g></g></svg>
