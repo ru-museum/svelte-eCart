d36e5e0bd949058213cae24e8a3ffc08b48c7728
ru-museum <museum_ru@yahoo.co.jp>
2021-01-15 11:44:14 +0900

    Create arrow-circle-up.svelte

---
+++ src/material-icons/arrow-circle-up.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let size = "1em";
+  export let color = "currentColor";
+  export let viewBox = "0 0 24 24";
+</script>
+
+<svg fill={color} width="{size}" height="{size}" viewBox="{viewBox}"><g><rect fill="none" height="24" width="24"/><path d="M12,20c-4.41,0-8-3.59-8-8s3.59-8,8-8s8,3.59,8,8S16.41,20,12,20 M12,22c5.52,0,10-4.48,10-10c0-5.52-4.48-10-10-10 C6.48,2,2,6.48,2,12C2,17.52,6.48,22,12,22L12,22z M11,12l0,4h2l0-4h3l-4-4l-4,4H11z"/></g></svg>
