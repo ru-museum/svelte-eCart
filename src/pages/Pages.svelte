1f0a986b33ce3f4452530f1b83a0acf446455d59
ru-museum <museum_ru@yahoo.co.jp>
2021-01-24 20:22:42 +0900

    Create Pages.svelte

---
+++ src/pages/Pages.svelte
@@ -0,0 +1,24 @@
+
+<script>
+   
+  export let page;
+ 	import md from 'marked';
+
+</script>
+
+<div class="main">
+
+   {@html md(user_pages[page-1].content)}
+    
+</div>
+
+<style>
+
+   .main{ 
+      padding:0 6px; 
+      height: 100%; 
+   }
+
+</style>
+    
+      
